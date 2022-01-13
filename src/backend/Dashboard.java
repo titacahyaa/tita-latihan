87623438d07973aef09164ab6d79d9fafb72a511
titacahyaa <pratistha2602@gmail.com>
2022-01-13 09:04:42 +0700

    menambahkan kode 1

--- src/backend/Dashboard.java
+++ src/backend/Dashboard.java
@@ -13,5 +13,6 @@ public class Dashboard {
     public static void main(String[] args) {
         System.out.println("Dashboard");
         System.out.println("Menu");
+        System.out.println("1");
     }
 }
