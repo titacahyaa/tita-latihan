2956f95b01de9db8993f2549156746eedf4e36b4
titacahyaa <pratistha2602@gmail.com>
2022-01-13 08:42:17 +0700

    initial project

---
+++ src/backend/Dashboard.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+
+/**
+ *
+ * @author titacahyaa
+ */
+public class Dashboard {
+    public static void main(String[] args) {
+        System.out.println("Dashboard");
+        System.out.println("Menu");
+    }
+}
